d9a38abe182b51511b1d4eed7f4ad52504872a50
forye <idanfonia2@gmail.com>
2023-12-16 14:54:24 +0200

    working version, 4 endpoints and tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,12 @@
 import pytest
 from app.main import app
+from locust import HttpUser, task, between
+
+MOCK_REQUEST_DATA = {
+        "venue_id": "8a61bb7",
+        "time_received": "2021-01-01T12:00:00",
+        "is_retail": True
+    }
 
 @pytest.fixture
 def client():
@@ -7,27 +14,17 @@ def client():
         yield client
 
 def test_predict_endpoint(client):
-    # Mock request data
-    mock_request_data = {
-        "venue_id": 123,
-        "time_received": "2021-01-01T12:00:00",
-        "is_retail": True
-    }
-
     # Send a POST request to the /predict endpoint
-    response = client.post('/predict', json=mock_request_data)
+    response = client.post('/predict', json=MOCK_REQUEST_DATA)
 
     # Check if the response is as expected
     assert response.status_code == 200
     assert "prediction" in response.json
+    # tbd do moore test according to label distribution and business logic
 
 
 # integration test
-
-import pytest
-from app.main import app
-
-@pytest.fixture
+@pytest.fixtures
 def client():
     with app.test_client() as client:
         yield client
@@ -35,32 +32,22 @@ def client():
 def test_complete_prediction_flow(client):
     # Assuming Redis is running and has necessary data
 
-    # Mock request data
-    mock_request_data = {
-        "venue_id": 123,
-        "time_received": "2021-01-01T12:00:00",
-        "is_retail": True
-    }
-
     # Send a POST request to the /predict endpoint
-    response = client.post('/predict', json=mock_request_data)
+    response = client.post('/predict', json=MOCK_REQUEST_DATA)
 
     # Check if the entire flow is completed successfully
     assert response.status_code == 200
     assert "prediction" in response.json
     # Further assertions can be made based on the expected response
+    assert response.json['prediction'] >= 0
+    assert response.json['prediction'] is not None
 
 
 # performance test
-from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 2)
 
     @task
     def predict_endpoint(self):
-        self.client.post("/predict", json={
-            "venue_id": "8a61bb7",
-            "time_received": "2021-01-01T12:00:00",
-            "is_retail": True
-        })
+        self.client.post("/predict", json=MOCK_REQUEST_DATA)
